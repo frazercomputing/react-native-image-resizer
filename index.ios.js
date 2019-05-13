2c348c50b36a5cf92004879fa52cc053f87b7fdb
Valeriy Gerasymenko <mrgerych@gmail.com>
2019-05-13 16:22:47 +0300

    grayscale in js

--- index.ios.js
+++ index.ios.js
@@ -25,4 +25,28 @@ export default {
       );
     });
   },
+  createGrayscaleResizedImage: (path, width, height, format, quality, rotation = 0, outputPath) => {
+    if (format !== 'JPEG' && format !== 'PNG') {
+      throw new Error('Only JPEG and PNG format are supported by createResizedImage');
+    }
+
+    return new Promise((resolve, reject) => {
+      NativeModules.ImageResizer.createResizedImage(
+        path,
+        width,
+        height,
+        format,
+        quality,
+        rotation,
+        outputPath,
+        (err, response) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(response);
+        }
+      );
+    });
+  },
 };
